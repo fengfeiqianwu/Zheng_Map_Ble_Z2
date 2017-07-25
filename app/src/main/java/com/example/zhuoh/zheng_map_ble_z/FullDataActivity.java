fec3f963e88824ba59e8fbb23f6f58f722664dc8
Fengfeiqianwu <991253061@qq.com>
2017-07-25 09:31:13 +0800

    修改了tablayout成块

--- app/src/main/java/com/example/zhuoh/zheng_map_ble_z/FullDataActivity.java
+++ app/src/main/java/com/example/zhuoh/zheng_map_ble_z/FullDataActivity.java
@@ -9,50 +9,111 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.design.widget.TabLayout;
 import android.os.Bundle;
+import android.view.View;
+
+import com.flyco.tablayout.CommonTabLayout;
+import com.flyco.tablayout.listener.CustomTabEntity;
+import com.flyco.tablayout.listener.OnTabSelectListener;
+
+import java.util.ArrayList;
+
+import UI.SimpleCardFragment;
+import entity.TabEntity;
+import utils.ViewFindUtils;
 
 public class FullDataActivity extends AppCompatActivity {
 
-    private static FullDataActivity instance;
-    public static FullDataActivity getInstance() {
-        return instance;
-    }
+    private Context mContext = this;
+    private ArrayList<Fragment> mFragments = new ArrayList<>();
+    private String[] mTitles = {"全网", "移2", "移3", "移4", "联2","联3","联4","电2","电4","WIFI"};
+    private ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
+    private View mDecorView;
+    private ViewPager mViewPager;
+    private PageFragment pageFragment;
+    private CommonTabLayout mTabLayout_8;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_data);
+        for (String title : mTitles) {
+            mFragments.add(SimpleCardFragment.getInstance("Switch ViewPager " + title));
+
+        }
+
+        mDecorView = getWindow().getDecorView();
+        mViewPager = ViewFindUtils.find(mDecorView, R.id.vp_2);
+        mViewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+
+        for (int i = 0; i < mTitles.length; i++) {
+            mTabEntities.add(new TabEntity(mTitles[i], 0, 0));
+        }
+        /** indicator圆角色块 */
+        mTabLayout_8 = ViewFindUtils.find(mDecorView, R.id.tl_8);
+        mTabLayout_8.setTabData(mTabEntities);
+        tl_2();
         //Fragment+ViewPager+FragmentViewPager组合的使用
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        /*ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
         MyFragmentPagerAdapter adapter = new MyFragmentPagerAdapter(getSupportFragmentManager(),
                 this);
         viewPager.setAdapter(adapter);
 
         //TabLayout
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
-        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);*/
     }
-    class MyFragmentPagerAdapter extends FragmentPagerAdapter {
-        public final int COUNT = 10;
-        private String[] titles = new String[]{"全网", "移2", "移3", "移4", "联2","联3","联4","电2","电4","W"};
-        private Context context;
-
-        public MyFragmentPagerAdapter(FragmentManager fm, Context context) {
+    private class MyPagerAdapter extends FragmentPagerAdapter {
+        public MyPagerAdapter(FragmentManager fm) {
             super(fm);
-            this.context = context;
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return PageFragment.newInstance(position + 1);
         }
 
         @Override
         public int getCount() {
-            return COUNT;
+            return mFragments.size();
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            return titles[position];
+            return mTitles[position];
         }
+
+        @Override
+        public Fragment getItem(int position) {
+            return PageFragment.newInstance(position+1);
+            //return mFragments.get(position);
+        }
+    }
+    private void tl_2() {
+        mTabLayout_8.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelect(int position) {
+                mViewPager.setCurrentItem(position);
+            }
+
+            @Override
+            public void onTabReselect(int position) {
+                /*if (position == 0) {
+                    mTabLayout_8.showMsg(0, mRandom.nextInt(100) + 1);*/
+//                    UnreadMsgUtils.show(mTabLayout_2.getMsgView(0), mRandom.nextInt(100) + 1);
+            }
+        });
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                mTabLayout_8.setCurrentTab(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        mViewPager.setCurrentItem(0);
     }
 }
